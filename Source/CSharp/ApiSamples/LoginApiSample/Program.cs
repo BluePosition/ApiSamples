c1b55f003bda6fa7be02a739a4675f8a9f56ad7d
Nicklas Møller Jepsen <nmj@blueposition.com>
2016-02-23 13:09:18 +0100

    Added retry functionality to the login app.

--- Source/CSharp/ApiSamples/LoginApiSample/Program.cs
+++ Source/CSharp/ApiSamples/LoginApiSample/Program.cs
@@ -7,22 +7,38 @@ using Newtonsoft.Json;
 
 namespace BluePosition.Samples.LoginApi
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the Ocp Api Management Subscription Key (go to developer.cloud.mobileservices.dk to get a subscription key):");
-            var ocpApimSubscriptionKey = Console.ReadLine();
-            Console.WriteLine("Please enter your Scale user name in the format user@vkxxxxxx.hvoip.dk:");
-            var username = Console.ReadLine();
-            Console.WriteLine("Password:");
-            var password = Console.ReadLine();
+            var retry = true;
+            do
+            {
+                Console.WriteLine("Please enter the Ocp Api Management Subscription Key (go to developer.cloud.mobileservices.dk to get a subscription key):");
+                var ocpApimSubscriptionKey = Console.ReadLine();
+                Console.WriteLine("Please enter your Scale user name in the format user@vkxxxxxx.hvoip.dk:");
+                var username = Console.ReadLine();
+                Console.WriteLine("Password:");
+                var password = Console.ReadLine();
 
-            var loginTask = new LoginTask(ocpApimSubscriptionKey);
-            var token = loginTask.Execute(username, password).Result;
-            Console.WriteLine($"Logged in as {username} with token: {token}");
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
+                var loginTask = new LoginTask(ocpApimSubscriptionKey);
+                var token = loginTask.Execute(username, password).Result;
+                if (string.IsNullOrEmpty(token))
+                {
+                    Console.WriteLine("Press q to quit or any other key to retry.");
+                    var key = Console.ReadKey(true);
+                    if (key.KeyChar == 'q')
+                        retry = false;
+                }
+                else
+                {
+                    Console.WriteLine($"Logged in as {username} with token: {token}");
+                    Console.WriteLine("Press any key to exit.");
+                    Console.ReadKey();
+                    retry = false;
+                }
+            }
+            while (retry);
         }
     }
 }
